95d2c64969f1e890f0a29da462efbdac32dd73ae
Umar <mohammadumar28@gmail.com>
2021-03-15 17:42:02 +0530

    Optimize code in routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create], :defaults => { :format => 'json' }
+  defaults format: :json do
+    resources :users, only: [:create]
+    resources :cars, only: [:index, :create, :show, :destroy]
+    resources :appointments, only: [:index, :create, :destroy]
+  end
   resources :sessions, only: [:create]
-  resources :cars, only: [:index, :create, :show, :destroy], :defaults => { :format => 'json' } 
   resources :pictures, only: [:create, :destroy]
-  resources :appointments, only: [:index, :create, :destroy], :defaults => { :format => 'json' } 
 end
